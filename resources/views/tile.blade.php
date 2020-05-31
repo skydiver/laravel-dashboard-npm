ea778167a86dd473c8cae24a9050acdfb06b17aa
Martín M <skydiver@users.noreply.github.com>
2020-05-31 17:38:47 -0300

    tile view enhancements

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -1,9 +1,11 @@
 <x-dashboard-tile :position="$position">
-    <div class="text-center text-gray-800">
-        <div class="text-gray-600 text-sm">{{ $package }}</div>
-        <div class="text-xs text-gray-500" style="font-size: 0.5rem">Monthly Downloads</div>
-        <div class="my-2 font-semibold text-3xl tracking-wide leading-none">{{ $packageInfo['downloads'] }}</div>
-        <div class="text-xs text-gray-600" style="font-size: 0.6rem">{{ $packageInfo['start'] }} ~ {{ $packageInfo['end'] }}</div>
-        <div class="mt-2 text-xs text-gray-400 text-right" style="font-size: 0.5rem">Updated at: {{ $packageInfo['updated_at'] }}</div>
+    <div class="relative h-full flex items-center">
+        <div class="w-full text-center text-gray-800">
+            <div class="text-gray-600">{{ $package }}</div>
+            <div class="text-xs text-gray-500" style="font-size: 0.5rem">Monthly Downloads</div>
+            <div class="my-3 font-semibold text-3xl tracking-wide leading-none">{{ number_format($packageInfo['downloads']) }}</div>
+            <div class="text-xs text-gray-600" style="font-size: 0.6rem">{{ $packageInfo['start'] }} ~ {{ $packageInfo['end'] }}</div>
+        </div>
+        <div class="absolute bottom-0 right-0 text-xs text-gray-500 text-right" style="font-size: 0.5rem">Updated at: {{ $packageInfo['updated_at'] }}</div>
     </div>
 </x-dashboard-tile>
